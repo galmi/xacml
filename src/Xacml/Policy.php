339025c86a6ba21967d9f16d8f493d37fe3c7d8c
galmi <galmi.ru@gmail.com>
2015-12-22 13:01:23 +0700

    added evaluate method to Policy class

--- src/Xacml/Policy.php
+++ src/Xacml/Policy.php
@@ -5,7 +5,12 @@ namespace Galmi\Xacml;
 
 use Galmi\Xacml\Algorithm\CombiningAlgorithm;
 
-class Policy
+/**
+ * The <Policy> class is the smallest entity that SHALL be presented to the PDP for evaluation.
+ *
+ * @author Ildar Galiautdinov <ildar@galmi.ru>
+ */
+class Policy implements Evaluable
 {
 
     protected $id;
@@ -43,6 +48,8 @@ class Policy
     protected $combiningAlgorithm;
 
     /**
+     * Getter for Version
+     *
      * @return number
      */
     public function getVersion()
@@ -51,14 +58,21 @@ class Policy
     }
 
     /**
+     * Setter for Version
+     *
      * @param number $version
+     * @return $this
      */
     public function setVersion($version)
     {
         $this->version = $version;
+
+        return $this;
     }
 
     /**
+     * Getter for Description
+     *
      * @return string
      */
     public function getDescription()
@@ -67,14 +81,21 @@ class Policy
     }
 
     /**
+     * Setter for Description
+     *
      * @param string $description
+     * @return $this
      */
     public function setDescription($description)
     {
         $this->description = $description;
+
+        return $this;
     }
 
     /**
+     * Getter for Target
+     *
      * @return Target
      */
     public function getTarget()
@@ -83,14 +104,21 @@ class Policy
     }
 
     /**
+     * Setter for Target
+     *
      * @param Target $target
+     * @return $this
      */
     public function setTarget($target)
     {
         $this->target = $target;
+
+        return $this;
     }
 
     /**
+     * Getter for Combining algorithm
+     *
      * @return CombiningAlgorithm
      */
     public function getCombiningAlghoritm()
@@ -99,14 +127,21 @@ class Policy
     }
 
     /**
+     * Setter Combining algorithm
+     *
      * @param CombiningAlgorithm $combiningAlgorithm
+     * @return $this
      */
     public function setCombiningAlghoritm($combiningAlgorithm)
     {
         $this->combiningAlgorithm = $combiningAlgorithm;
+
+        return $this;
     }
 
     /**
+     * Getter for Rule
+     *
      * @return Rule[]
      */
     public function getRules()
@@ -115,6 +150,8 @@ class Policy
     }
 
     /**
+     * Add Rule
+     *
      * @param Rule $rule
      * @return $this
      */
@@ -128,6 +165,8 @@ class Policy
     }
 
     /**
+     * Remove Rule
+     *
      * @param Rule $rule
      * @return $this
      */
@@ -140,8 +179,70 @@ class Policy
             }
 
             unset($this->rules[$key]);
+            $this->rules = array_values($this->rules);
         }
 
         return $this;
+    }
+
+    /**
+     *  ---------------------------------------------------------------------------
+     * |    Target       | Rule values |              Policy Value                 |
+     *  ---------------------------------------------------------------------------
+     * | “Match”         | Don’t care  | Specified by the rule-combining algorithm |
+     * | “No-match”      | Don’t care  | “NotApplicable”                           |
+     * | “Indeterminate” | See Table 7 | See Table 7                               |
+     *  ---------------------------------------------------------------------------
+     *
+     * Table 7
+     *  --------------------------------------------------------
+     * | Combining algorithm Value | Policy set or policy Value |
+     *  --------------------------------------------------------
+     * | “NotApplicable”           | “NotApplicable”            |
+     * | “Permit”                  | “Indeterminate{P}”         |
+     * | “Deny”                    | “Indeterminate{D}”         |
+     * | “Indeterminate”           | “Indeterminate{DP}”        |
+     * | “Indeterminate{DP}”       | “Indeterminate{DP}”        |
+     * | “Indeterminate{P}”        | “Indeterminate{P}”         |
+     * | “Indeterminate{D}”        | “Indeterminate{D}”         |
+     *  --------------------------------------------------------
+     *
+     * @inheritdoc
+     */
+    public function evaluate(Request $request)
+    {
+        $combiningAlgorithmDecision = null;
+        $decision = Decision::NOT_APPLICABLE;
+        try {
+            if ($this->getTarget() == null || $this->getTarget()->evaluate($request) === Match::MATCH) {
+                $combiningAlgorithmDecision = $this->combiningAlgorithm->evaluate($this->rules);
+                $decision = $combiningAlgorithmDecision;
+            }
+        } catch (\Exception $e) {
+            switch ($combiningAlgorithmDecision) {
+                case (Decision::NOT_APPLICABLE):
+                    $decision = Decision::NOT_APPLICABLE;
+                    break;
+                case (Decision::PERMIT):
+                    $decision = Decision::INDETERMINATE_P;
+                    break;
+                case (Decision::DENY):
+                    $decision = Decision::INDETERMINATE_D;
+                    break;
+                case (Decision::INDETERMINATE_D_P):
+                    $decision = Decision::INDETERMINATE_D_P;
+                    break;
+                case (Decision::INDETERMINATE_P):
+                    $decision = Decision::INDETERMINATE_P;
+                    break;
+                case (Decision::INDETERMINATE_D):
+                    $decision = Decision::INDETERMINATE_D;
+                    break;
+                default:
+                    $decision = Decision::INDETERMINATE_D_P;
+            }
+        }
+
+        return $decision;
     }
 }
